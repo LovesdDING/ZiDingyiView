fb51bc2a085c27cc812b02ac4dbadfc9270593ec
LovesdDING <451972500@qq.com>
2017-08-29 10:57:45 +0800

    Initial commit

---
+++ app/src/main/java/patient/yilin/com/testzidingyiview/MainActivity.java
@@ -0,0 +1,13 @@
+package patient.yilin.com.testzidingyiview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
